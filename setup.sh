f9dcd25c52759a5bb56241ed465fce176c1055f1
iopraveen <saxep01@gmail.com>
2018-04-21 00:11:36 -0500

    Mod: terminate script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sudo ln -s terminate.sh /usr/bin/terminate
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+echo $SCRIPTPATH
+
+
+sudo ln -s $SCRIPTPATH/terminate.sh /usr/bin/terminate
+sudo ln -s $SCRIPTPATH/terminate.sh /usr/local/bin/terminate
 chmod +x  terminate.sh
 
