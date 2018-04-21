90d6b3661dd2eb749e74d61b817e54c246735d68
iopraveen <38472912+iopraveen@users.noreply.github.com>
2018-04-21 01:17:55 -0400

    Mod: change script terminate

--- terminate.sh
+++ terminate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # requires the correct region set via the cli `aws configure` or in ~/.aws/config 
 
-ec2-terminate-instances $(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+aws ec2 terminate-instances --instance-ids `curl http://169.254.169.254/latest/meta-data/instance-id`
 
